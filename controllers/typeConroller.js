fff6b37ac6b96dec0a911905f29115ef46103cc9
Oguzhan Ulutas <mkoulutas@gmail.com>
2023-09-26 15:00:46 +0300

    Add dummy callbacks for all the type routes

---
+++ controllers/typeConroller.js
@@ -0,0 +1,42 @@
+const asyncHandler = require('express-async-handler');
+const Type = require('../models/type');
+
+// Display list of all the types
+exports.typeList = asyncHandler(async (req, res, next) => {
+  res.send('NOT IMPLEMENTED: Type List');
+});
+
+// Display detail page for a specific type
+exports.typeDetail = asyncHandler(async (req, res, next) => {
+  res.send(`NOT IMPLEMENTED: Type Detail: ${req.params.id}`);
+});
+
+// Display type create form on GET
+exports.typeCreateGet = asyncHandler(async (req, res, next) => {
+  res.send('NOT IMPLEMENTED: Type Create GET');
+});
+
+// Handle type create on POST.
+exports.typeCreatePost = asyncHandler(async (req, res, next) => {
+  res.send('NOT IMPLEMENTED: type create POST');
+});
+
+// Display type delete form on GET.
+exports.typeDeleteGet = asyncHandler(async (req, res, next) => {
+  res.send('NOT IMPLEMENTED: Type delete GET');
+});
+
+// Handle type delete on POST.
+exports.typeDeletePost = asyncHandler(async (req, res, next) => {
+  res.send('NOT IMPLEMENTED: type delete POST');
+});
+
+// Display type update form on GET.
+exports.typeUpdateGet = asyncHandler(async (req, res, next) => {
+  res.send('NOT IMPLEMENTED: type update GET');
+});
+
+// Handle type update on POST.
+exports.typeUpdatePost = asyncHandler(async (req, res, next) => {
+  res.send('NOT IMPLEMENTED: type update POST');
+});
